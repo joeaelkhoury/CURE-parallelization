3c674505e2a286d9d804190d024c61b762a12358
Yusuke_Sugihara <53958577+Yusuke-Sugihara@users.noreply.github.com>
2024-02-15 09:35:45 +0100

    Update cure_auto.sh

--- src/model/cure_auto.sh
+++ src/model/cure_auto.sh
@@ -11,19 +11,19 @@ cd $PBS_O_WORKDIR
 
 # Define list of data files to be processed
 data_files=("2000data.txt")
-
 # Specify the numbers of processes to be used for each execution
-process_counts=(1 4 8)
+process_counts=(1 2 4 8 16)
 
 # Loop over each data file
 for data_file in "${data_files[@]}"; do
-    # Compile the C program for each data file
+    # Compile the C program for each data file and redirect compile output to a file
     executable_name="cure_auto_${data_file%.txt}"
-    mpicc -g -Wall -fopenmp cure_auto.c -std=c99 -o "$executable_name" -lm -DDATA_FILE=\"$data_file\"
+    compile_output_file="compile_output_${data_file%.txt}.txt"
+    mpicc -g -Wall -fopenmp cure_auto.c -std=c99 -o "$executable_name" -lm -DDATA_FILE=\"$data_file\" > "$compile_output_file" 2>&1
 
     # Check if compilation was successful
     if [ ! -f "./$executable_name" ]; then
-        echo "Compilation failed or $executable_name not found."
+        echo "Compilation failed or $executable_name not found. See $compile_output_file for details."
         exit 1
     fi
 
